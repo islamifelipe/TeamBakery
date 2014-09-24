49b04a5ef1471d2078b82764fe75d2f9862fe0b5
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2014-09-24 19:24:30 -0300

    Futucadas em alguns valores

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,43 +1,47 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float maxVelocity, jumpStrenght;
-	public Vector3 InitialScale;
-	private float Velocity;
-	public Animator myAnim;
+	public float maxVerSpeed, jumpStrenght, sprintBonus;
+	public Vector3 initialScale;
+	public Animator PCAnim;
+	private float horSpeed, verSpeed;
+	private bool jump;
+
+	void OnCollisionEnter(Collision col){// Checa se Mario esta no chao
+		if(col.gameObject.name == "floor") jump = true;
+		else jump = false;
+		Debug.Log("Hello", gameObject);
+	}
 
 	// Use this for initialization
-	void Start () {
-	
-		InitialScale = transform.localScale;
+	void Start(){
+		Debug.Log("Hello", gameObject);
+		initialScale = transform.localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-		Velocity = 0;
-
-		if(Input.GetKey(KeyCode.RightArrow)){
-			Velocity = maxVelocity;				
-			transform.localScale = InitialScale;
-
+		horSpeed = 0;
+		verSpeed = rigidbody2D.velocity.y;
+		if(Input.GetKey(KeyCode.RightArrow)){ // Andar para a direita
+			horSpeed = maxVerSpeed;
+			if(Input.GetKey(KeyCode.LeftShift)) horSpeed *= sprintBonus;
+			transform.localScale = initialScale;
 		}
-
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			Velocity = -1*maxVelocity;	
-			transform.localScale = new Vector3(InitialScale.x*-1, InitialScale.y, InitialScale.z);
+		else if(Input.GetKey (KeyCode.LeftArrow)){ // Andar para a esquerda
+			horSpeed = -maxVerSpeed;
+			if(Input.GetKey(KeyCode.LeftShift)) horSpeed *= sprintBonus;
+			transform.localScale = new Vector3(-initialScale.x, initialScale.y, initialScale.z);
 		}
-
-		if (Input.GetKey (KeyCode.UpArrow)) {
-				
+		if (Input.GetKey (KeyCode.UpArrow) && jump == true){ // Salto
+			verSpeed = jumpStrenght;
 			rigidbody2D.AddForce(new Vector2(0, jumpStrenght));
 		}
+		PCAnim.SetFloat ("horSpeed", Mathf.Abs(horSpeed));
+		PCAnim.SetFloat ("verSpeed", Mathf.Abs(verSpeed));
 
-		rigidbody2D.velocity = new Vector2 (Velocity, rigidbody2D.velocity.y);
-
-		myAnim.SetFloat ("Velocity", Mathf.Abs(Velocity));
-
+		rigidbody2D.velocity = new Vector2 (horSpeed, rigidbody2D.velocity.y);
 	}
 }
