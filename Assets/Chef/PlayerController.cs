31b87b95e43a0de753b6c64cbf53d3250705e2b1
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2014-10-07 13:35:58 -0300

    Tentando ajeitar a animação

--- Assets/Chef/PlayerController.cs
+++ Assets/Chef/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public AudioClip changeWeaponSound, attackSound;
 	public float maxHorSpeed, jumpStrenght, sprintBonus, maxSprintSpeed, sprintFadeSpeed, bounceTest;
 	public float horDeacc, horAcc, shakeIntensity;
 	public bool attack;
@@ -23,6 +24,7 @@ public class PlayerController : MonoBehaviour {
 		colliderCenter = chefCollider.center.y;
 		weaponChoice = 0;
 		camShake = 0;
+		cam = GameObject.Find ("mainCam");
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
@@ -44,22 +46,22 @@ public class PlayerController : MonoBehaviour {
 		verSpeed = rigidbody2D.velocity.y;
 		
 		if (camShake == 1 || camShake == 2 || camShake == 5 || camShake == 6) {
-				cam = GameObject.Find ("mainCam");
 				cam.transform.position = new Vector3 (cam.transform.position.x, cam.transform.position.y + shakeIntensity, cam.transform.position.z);
 				camShake++;
 		}
 		else if (camShake == 3 || camShake == 4) {
-				cam = GameObject.Find ("mainCam");
-				cam.transform.position = new Vector3 (cam.transform.position.x, cam.transform.position.y - shakeIntensity * 2, cam.transform.position.z);
+				cam.transform.position = new Vector3 (cam.transform.position.x, cam.transform.position.y + shakeIntensity * 2, cam.transform.position.z);
 				camShake++;		
 		}
 		else if (camShake == 7) camShake = 0;
 
 		if (Input.GetKeyDown(KeyCode.Q)) {
+			AudioSource.PlayClipAtPoint(changeWeapon, transform.position);
 			if(weaponChoice == 0) weaponChoice = 1;
 			else weaponChoice = 0;
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
+			AudioSource.PlayClipAtPoint(changeWeapon, transform.position);
 			duck = true;
 			chefCollider.size = new Vector2 (chefCollider.size.x, colliderSize-(float)0.4);
 			chefCollider.center = new Vector2 (chefCollider.center.x, colliderCenter-(float)0.2);
@@ -68,7 +70,10 @@ public class PlayerController : MonoBehaviour {
 			chefCollider.size = new Vector2 (chefCollider.size.x, colliderSize);
 			chefCollider.center = new Vector2 (chefCollider.center.x, colliderCenter);
 		}
-		if (Input.GetAxis ("Attack") > 0 && !duck) attack = true;
+		if (Input.GetAxis ("Attack") > 0 && !duck) {
+			AudioSource.PlayClipAtPoint(attackSound, transform.position);
+			attack = true;
+		}
 		if(Input.GetAxis("Horizontal") > 0 && !duck){ // Andar para a direita
 			if(horSpeed < 0) flip = true;
 			horSpeed += horAcc;
@@ -89,16 +94,17 @@ public class PlayerController : MonoBehaviour {
 			}
 			transform.localScale = new Vector3(-initialScale.x, initialScale.y, initialScale.z);
 		}
-		if(Input.GetAxis("Jump") > 0 && verSpeed < 0.2 && !duck && jump){ // Salto
+		if(Input.GetAxis("Jump") > 0 && verSpeed == 0 && !duck && jump){ // Salto
 			jump = false;
 			verSpeed = jumpStrenght;
 			rigidbody2D.AddForce(new Vector2(0, jumpStrenght));
 		}
 		PCAnim.SetFloat ("horSpeed", Mathf.Abs(horSpeed));
-		PCAnim.SetFloat ("verSpeed", Mathf.Abs(verSpeed));
+		PCAnim.SetFloat ("verSpeed", verSpeed);
 		PCAnim.SetBool ("duck", duck);
 		PCAnim.SetBool ("flip", flip);
 		PCAnim.SetBool ("attack", attack);
+		PCAnim.SetBool ("jump", jump);
 		PCAnim.SetInteger ("attackType", weaponChoice);
 		rigidbody2D.velocity = new Vector2 (horSpeed, rigidbody2D.velocity.y);
 	}
