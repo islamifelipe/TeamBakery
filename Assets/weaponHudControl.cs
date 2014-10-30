d197ba98a87bcdceeb7328e55a85c4c0da74249a
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2014-10-30 18:04:13 -0300

    Nova cena de teste
    Criação de uma nova cena de teste para tentar fazer uma fase para o jogo

---
+++ Assets/weaponHudControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class weaponHudControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void showWeaponHud(){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
