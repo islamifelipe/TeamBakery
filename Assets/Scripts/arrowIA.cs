0b31b4b2e06730296a89ed97b2631d9002d0af48
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2014-12-04 08:27:01 -0300

    Rosquinha

--- Assets/Scripts/arrowIA.cs
+++ Assets/Scripts/arrowIA.cs
@@ -3,20 +3,25 @@ using System.Collections;
 
 public class arrowIA : MonoBehaviour {
 	public GameObject chef;
-	private bool direction;
 	public float maxSpeed;
 	public float acc;
-
-	void Start () {	
+	private Vector2 scale;
+	
+	void Start () {
+		scale = this.transform.localScale;
 	}
 	
 	void Update () {
 		if(Time.timeScale == 1){
 			if (chef.transform.position.x < this.transform.position.x && this.rigidbody2D.velocity.x < maxSpeed){
-				this.rigidbody2D.AddForce(new Vector2(-acc, 0));	
+				this.rigidbody2D.AddForce(new Vector2(-acc, 0));
+				this.transform.localScale = new Vector2(-scale.x, scale.y);
+				this.GetComponent<enemyInfo>().direction = -1;
 			}
 			else if(this.rigidbody2D.velocity.x > -maxSpeed){
 				this.rigidbody2D.AddForce(new Vector2(acc, 0));
+				this.transform.localScale = new Vector2(scale.x, scale.y);
+				this.GetComponent<enemyInfo>().direction = 1;
 			}
 		}
 	}
